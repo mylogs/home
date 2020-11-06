9ea389cd23e81a8e72795fd4005e3a7a7ad3d5c2
mrphuongorg <mrphuongorg@gmail.com>
2020-11-07 02:03:06 +0700

    07/11/2020

---
+++ js/jquery-core-sri-modal.js
@@ -0,0 +1,55 @@
+$( function() {
+	// Store modal templates
+	var modalTemplate = $( "#sri-modal-template" )[ 0 ].outerHTML;
+
+	// Show modal on click
+	$( "body" ).on( "click", ".open-sri-modal", function( event ) {
+		if ( event.ctrlKey || event.metaKey ) {
+			return;
+		}
+
+		$( replace( modalTemplate, {
+			link: /*document.location.origin + */$( this ).attr( "href" ),
+			hash: $( this ).attr( "data-hash" )
+		} ) ).removeAttr( "id" ).appendTo( "body" ).dialog( {
+			modal: true,
+			resizable: false,
+			width: 830,
+			dialogClass: "sri-modal",
+			draggable: false,
+			close: function() {
+				$( this ).remove();
+			}
+		} );
+
+		$('.sri-modal-copy-btn')
+			.tooltip()
+			.on( "click", function() {
+				var buttonElem = $( this );
+				clipboard
+					.writeText( buttonElem.attr( "data-clipboard-text" ) )
+					.then( function() {
+						buttonElem
+							.tooltip( "option", "content", "Copied!" )
+							.one( "mouseout", function() {
+								buttonElem.tooltip( "option", "content", "Copy to clipboard!" );
+							} );
+					} )
+					.catch( function() {
+						buttonElem
+							.tooltip( "option", "content", "Copying to clipboard failed!" )
+							.one( "mouseout", function() {
+								buttonElem.tooltip( "option", "content", "Copy to clipboard!" );
+							} );
+					} );
+			} );
+		event.preventDefault();
+	} );
+
+	// Helper functions
+	function replace ( string, values ) {
+		return string.replace( /\{\{([^}]+)}}/g, function( _, key ) {
+			return values[key];
+		} );
+	}
+} );
